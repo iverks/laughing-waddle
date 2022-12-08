436f7872354e39311f24751c2cc44ffd739375a4
Documenter.jl <documenter@juliadocs.github.io>
2022-12-08 13:47:48 +0000

    build based on 75d0985

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"#Documentation-for-SintPowerCase.jl","page":"Documentation for SintPowerCase.jl","title":"Documentation for SintPowerCase.jl","text":"","category":"section"},{"location":"","page":"Documentation for SintPowerCase.jl","title":"Documentation for SintPowerCase.jl","text":"This is a package intended for reading in power system data from various formats and storing it in dataframes. The dataframes should be compatible with the MATPOWER format.","category":"page"}]
 }
