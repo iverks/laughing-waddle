3cd85afa43e41d897fbb0bdce2172ca82c137c76
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2023-02-06 14:57:42 +0100

    Added push_bus!

--- src/SintPowerCase.jl
+++ src/SintPowerCase.jl
@@ -4,7 +4,7 @@ include("case_format.jl")
 export Case, get_n_buses, get_islanded_buses, get_bus, get_reliability_data
 
 include("load_gen.jl")
-export get_power_injection_vector, is_gen_bus, get_load_indices, get_gen_indices, is_load_bus, get_gen_buses_power, get_load_buses_power, get_n_os, create_random_states!, set_gen_bus_idx!, set_load_bus_idx!, set_bus_idx!
+export get_power_injection_vector, is_gen_bus, get_load_indices, get_gen_indices, is_load_bus, get_gen_buses_power, get_load_buses_power, get_n_os, create_random_states!, set_gen_bus_idx!, set_load_bus_idx!, set_bus_idx!, push_bus!
 
 include("branch.jl")
 export get_line_lims_pu, get_n_lines, take_out_line!, is_switch, get_switch, is_indicator, get_branch 
