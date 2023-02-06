c5cd003c1e565c1d2d598a60d669399c24bbd3d6
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2023-02-06 14:39:28 +0100

    Added get_switch

--- src/SintPowerCase.jl
+++ src/SintPowerCase.jl
@@ -7,7 +7,7 @@ include("load_gen.jl")
 export get_power_injection_vector, is_gen_bus, get_load_indices, get_gen_indices, is_load_bus, get_gen_buses_power, get_load_buses_power, get_n_os, create_random_states!, set_gen_bus_idx!, set_load_bus_idx!, set_bus_idx!
 
 include("branch.jl")
-export get_line_lims_pu, get_n_lines, take_out_line!, is_switch, is_indicator, get_branch 
+export get_line_lims_pu, get_n_lines, take_out_line!, is_switch, get_switch, is_indicator, get_branch 
 
 include("matrices.jl")
 export get_incidence_matrix, get_island_incidence_matrix, get_dc_admittance_matrix, get_power_injection_vector_pu, get_bus_angle_vector, get_susceptance_vector
