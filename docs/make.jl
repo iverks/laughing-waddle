522a40e5b1b59aa6a417e1a402b61896d7761553
Iver Småge <iver.smage@sintef.no>
2024-06-18 12:43:33 +0200

    update repo url

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, SintPowerCase
 
 makedocs(sitename="SintPowerCase documentation")
-deploydocs(repo="github.com/Hofsmo/SintPowerCase.jl.git")
+deploydocs(repo="gitlab.sintef.no/power-system-asset-management/SintPowerCase.jl")
